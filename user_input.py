c00ec40f34b8f9e7234c60547fd5faad3fc5db6d
alfred-blip <126565333+alfred-blip@users.noreply.github.com>
2024-03-27 17:22:01 +0300

    user input program

---
+++ user_input.py
@@ -0,0 +1,11 @@
+# Get user input for name
+name = input("Please enter your name: ")
+
+# Get user input for age
+age = input("Please enter your age: ")
+
+# Get user input for location
+location = input("Please enter your location: ")
+
+# Print personalized message
+print("Hello {}, you are {} years old and live in {}.".format(name, age, location))
